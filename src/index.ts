a827b2be2bf8918931b68708aadb9582173c9129
Gustavo Criado <gustavoliracriado30@gmail.com>
2022-03-17 22:58:36 -0300

    desafio4 concluido

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import {Aluno} from "./Models/Aluno"
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+var readlineSync = require('readline-sync');
 
 /**
  * Server Activation
@@ -36,8 +38,22 @@ app.use(express.json());
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 
+	console.log('Bem vindo ao sistema das notinhas mil grau');
 
-	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
+	let alunos = []
+
+	for(var i = 1; i <= 3; i++)
+	{
+		
+		var NomeAluno = readlineSync.question('Digite o Nome do Aluno: ');
+		var NotaAluno = readlineSync.question('Digite o Idade do Aluno: ');
+		var NotaAluno = readlineSync.question('Digite o Nota do Aluno: ');
+
+		var primeiroAluno : Aluno = new Aluno(NomeAluno,NotaAluno, parseInt(NotaAluno));
+		alunos[i] = primeiroAluno
+	}
+
+	const somaDasNotas = alunos.reduce((a,b) => {return a += b.nota}, 0)
+	console.log(`a soma da nota dos 3 alunos é: ${somaDasNotas}`)
 	
 });
